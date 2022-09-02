a9cbbb5f321018bc10089037ca540ac7a484531c
Yasutaka Odo <yasutakaodo@gmail.com>
2022-09-03 02:08:29 +0900

    add color schemes

---
+++ src/utils/colors.js
@@ -0,0 +1,56 @@
+// https://github.com/emmabostian/fem-design-systems/blob/master/src/utils/colors.js
+
+export const primary = {
+  100: "#5286AB",
+  200: "00BBFF",
+  300: "#0F5173",
+  400: "#E6F4F1",
+  500: "#EEE8A9",
+};
+
+export const tetradic = {
+  100: "#5286AB",
+  200: "#52ABA3",
+  300: "#5259AB",
+  400: "#ABA352",
+  500: "#AB5259",
+};
+
+export const complementary = {
+  100: "#5286AB",
+  200: "#AB7752",
+};
+
+export const blue = {
+  100: "#3a36e0",
+  200: "#0804b8",
+  300: "#030086",
+  400: "#5f25a4",
+  500: "#050449",
+};
+export const green = {
+  100: "#529e66",
+  200: "#367b48",
+  300: "#276738",
+};
+
+export const yellow = {
+  100: "#e1c542",
+  200: "#cab23f",
+  300: "#b49e35",
+};
+
+export const red = {
+  100: "#d0454c",
+  200: "#b54248",
+  300: "#95353a",
+};
+
+export const neutral = {
+  100: "#ffffff",
+  200: "#f4f5f7",
+  300: "#e1e1e1",
+  400: "#737581",
+  500: "#4a4b53",
+  600: "#000000",
+};
