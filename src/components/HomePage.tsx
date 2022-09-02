c24edb9f25ecd6cab8c6eac0ffee77a2efbd8653
Yasutaka Odo <yasutakaodo@gmail.com>
2022-09-02 23:30:10 +0900

    changed language to jp

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,7 +3,10 @@ import { Box } from "@mui/material";
 export function HomePage() {
   return (
     <Box className="Home">
-      <h1>This is a Home.</h1>
+      <h1>
+        OCW Centralは日本国内の大学が提供するOCW (Open Courseware)のポータルサイトです。
+        京都大学情報学科の学生有志によって作成・運営されています。
+      </h1>
     </Box>
   );
 }
