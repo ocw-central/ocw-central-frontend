f48bac604f409fad3452166a69ee34f118ba1bba
Yasutaka Odo <yasutakaodo@gmail.com>
2022-09-04 19:14:33 +0900

    fixed path

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
+import "@/styles/App.css";
 import { theme } from "@/utils/themes";
 import { ThemeProvider } from "@emotion/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import { AboutPage } from "./components/AboutPage";
 import { Header } from "./components/common/Header";
 import { HomePage } from "./components/HomePage";
 import { SubjectPage } from "./components/SubjectPage";
 import { SubjectsPage } from "./components/SubjectsPage";
 import { UserGuidePage } from "./components/UserGuidePage";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
+        <GlobalStyles />
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
