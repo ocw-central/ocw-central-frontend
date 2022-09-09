44e7d14d26ad1d5a54df707bd126452b178ce1ea
Yasutaka Odo <yasutakaodo@gmail.com>
2022-09-10 03:37:31 +0900

    make font cooler

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,18 +1,18 @@
-import { Link } from "react-router-dom";
 import { ReactComponent as Img } from "@/assets/ocwc-owl.svg";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
 
-export function Logo(){
-    return (
-        <Box sx={{ flexGrow: 1 }} className="Header">
-            <Img style={{ width: "50px", height: "50px" }} />
+export function Logo() {
+  return (
+    <Box>
+      <Img style={{ width: "50px", height: "50px" }} />
 
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                <Link to="/" style={{ color: "#fff" }}>
-                OCW CENTRAL
-                </Link>
-            </Typography>
-        </Box>
-    )
+      <Typography variant="h6" sx={{ flexGrow: 1, fontFamily: "fantasy" }}>
+        <Link to="/" style={{ color: "#fff" }}>
+          OCW CENTRAL
+        </Link>
+      </Typography>
+    </Box>
+  );
 }
