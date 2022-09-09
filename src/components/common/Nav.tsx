71f9e25758828113269c1acccbdbbb17b9fe06d0
Yasutaka Odo <yasutakaodo@gmail.com>
2022-09-10 03:36:53 +0900

    coordinate nav items horizontally

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,25 +1,26 @@
+import styles from "@/styles/Nav.module.css";
 import { Link } from "react-router-dom";
 
-export function Nav(){
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">
-                        <a>Home</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/about">
-                        <a>このサイトについて</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/guideline">
-                        <a>利用ガイド</a>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-    )
+export function Nav() {
+  return (
+    <nav>
+      <ul className={styles.list}>
+        <li>
+          <Link to="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link to="/about">
+            <a>このサイトについて</a>
+          </Link>
+        </li>
+        <li>
+          <Link to="/guideline">
+            <a>利用ガイド</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
