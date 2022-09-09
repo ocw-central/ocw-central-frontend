6583b5cf726f66f0e7c2ee02a93e101bcedad9c8
Yasutaka Odo <yasutakaodo@gmail.com>
2022-09-10 03:38:26 +0900

    improve search bar apperance

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,24 +1,13 @@
-import { ReactComponent as Logo } from "@/assets/ocwc-owl.svg";
-import styled from "@emotion/styled";
+import styles from "@/styles/utils.module.css";
 import SearchIcon from "@mui/icons-material/Search";
-import { alpha, InputBase } from "@mui/material";
+import { InputBase } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
+import { alpha, styled } from "@mui/material/styles";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import { Link } from "react-router-dom";
+import { Logo } from "src/components/common/Logo";
 import { Nav } from "src/components/common/Nav";
-
-const Search = styled("div")(() => ({
-  position: "relative",
-  marginLeft: 0,
-  backgroundColor: alpha("#fff", 0.15),
-  "&:hover": {
-    backgroundColor: alpha("#fff", 0.25),
-  },
-}));
 
 const ToSubjectsButton = styled(Button)(() => ({
   backgroundColor: alpha("#fff", 0.9),
@@ -27,34 +16,64 @@ const ToSubjectsButton = styled(Button)(() => ({
   },
 }));
 
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
 export function Header() {
   return (
-    <Box sx={{ flexGrow: 1 }} className="Header">
+    <Box sx={{ flexGrow: 1 }} className={styles.sideBySide}>
       <AppBar position="static" sx={{ bgcolor: "primary.light" }}>
+        <Logo />
         <Toolbar>
-          <Logo style={{ width: "50px", height: "50px" }} />
-          <ToSubjectsButton>
-            <Link to="/subjects">講義検索</Link>
-          </ToSubjectsButton>
-
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link to="/" style={{ color: "#fff" }}>
-              OCW Central
-            </Link>
-          </Typography>
-          <Nav />
           <Search>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label=""
-              sx={{ mr: 2 }}
-            >
+            <SearchIconWrapper>
               <SearchIcon />
-            </IconButton>
-            <InputBase placeholder="講義名を検索" />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
           </Search>
+          <Nav />
         </Toolbar>
       </AppBar>
     </Box>
