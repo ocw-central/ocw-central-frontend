a588c0897caec7c111312776e33d240a913ef025
Yasutaka Odo <YasutakaOdo@gmail.com>
2022-09-12 00:08:13 +0900

    change wording (#28)
    * modify message
    
    * change menubar list

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,8 +4,7 @@ export function HomePage() {
   return (
     <Box className="Home">
       <h1>
-        OCW Centralは日本国内の大学が提供するOCW (Open Courseware)のポータルサイトです。
-        京都大学情報学科の学生有志によって作成・運営されています。
+        OCW Centralは日本国内の大学が提供するOCW (Open Courseware)のポータルサイトです。CCライセンスに準拠した教育資源の保全とアクセスの向上を目的として、京都大学情報学科の学生有志によって制作運営されています。
       </h1>
     </Box>
   );
