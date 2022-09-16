89d271cc7ae0161eb28894979ba65c0d1c96a023
Yasutaka Odo <yasutakaodo@gmail.com>
2022-09-17 02:53:33 +0900

    align icons

--- src/components/common/Contact.tsx
+++ src/components/common/Contact.tsx
@@ -1,13 +1,21 @@
 // https://github.com/ebisucom/next-react-website/blob/main/blog/components/contact.js
+import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { Social } from "@/components/common/Social";
 import styles from "@/styles/contact.module.css";
 
 export function Contact() {
   return (
     <div className={styles.stack}>
       <h3 className={styles.heading}>Contact</h3>
-      <Social iconSize="30px" />
+      <a href="https://twitter.com/ocwcentral">
+        <FontAwesomeIcon icon={faTwitter} />
+        <span className="sr-only">Twitter</span>
+      </a>
+      <a href="https://github.com/kafugen">
+        <FontAwesomeIcon icon={faGithub} />
+        <span className="sr-only">GitHub</span>
+      </a>
       <address>ocwcentral.kyoto@gmail.com</address>
     </div>
   );
