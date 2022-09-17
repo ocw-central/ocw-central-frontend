93453a1808d2eb88852f2ceb7f8be6206754bbad
Yasutaka Odo <yasutakaodo@gmail.com>
2022-09-18 03:45:34 +0900

    remove unused css files

--- src/components/common/Contact.tsx
+++ src/components/common/Contact.tsx
@@ -1,14 +1,9 @@
 // https://github.com/ebisucom/next-react-website/blob/main/blog/components/contact.js
-import styles from "@/styles/contact.module.css";
-import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Box } from "@mui/material";
 
 export function Contact() {
   return (
-    <div className={styles.stack}>
-      <h3 className={styles.heading}>Contact</h3>
-
+    <div className="Contact">
+      <h3>Contact</h3>
       <address>feedback@ocwcentral.com</address>
     </div>
   );
