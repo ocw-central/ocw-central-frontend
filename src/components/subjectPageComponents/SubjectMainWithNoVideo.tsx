cb24cf1cb27d30291679092f8e4e6e7225ab0c0e
Yasutaka Odo <YasutakaOdo@gmail.com>
2022-09-24 20:58:26 +0900

    add accordion

--- src/components/subjectPageComponents/SubjectMainWithNoVideo.tsx
+++ src/components/subjectPageComponents/SubjectMainWithNoVideo.tsx
@@ -5,6 +5,14 @@ import { SubjectDetails } from "@/components/subjectPageComponents/SubjectDetail
 import { SubjectResources } from "@/components/subjectPageComponents/SubjectResources";
 import { SubjectSyllabus } from "@/components/subjectPageComponents/SubjectSyllabus";
 
+import { Typography } from "@mui/material";
+//import { ChapterBox } from "./subjectPageComponents/ChapterBox";
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+
 type Subject = {
   __typename?: "Subject" | undefined;
   id: string;
@@ -106,9 +114,46 @@ export function SubjectMainWithNoVideo(props: Props) {
   return (
     <Box className="Subject">
       <Box className="MainBox">
-        {hasResources && <SubjectResources subject={props.subject} />}
-        {hasDetails && <SubjectDetails subject={props.subject} />}
-        {hasSyllabus && <SubjectSyllabus subject={props.subject} />}
+        {hasResources && (
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="resource"
+              id="resource"
+            >
+              <Typography>講義資料</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <SubjectResources subject={props.subject} />
+            </AccordionDetails>
+          </Accordion>
+        )}
+        {hasDetails && (
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="details"
+              id="details"
+            >
+              <Typography>講義詳細</Typography>
+            </AccordionSummary>
+            <SubjectDetails subject={props.subject} />
+          </Accordion>
+        )}
+        {hasSyllabus && (
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="syllabus"
+              id="syllabus"
+            >
+              <Typography>シラバス</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <SubjectSyllabus subject={props.subject} />
+            </AccordionDetails>
+          </Accordion>
+        )}
       </Box>
     </Box>
   );
