f10e21c497c1f229636efd9a9798e20a9db8b530
Yasutaka Odo <YasutakaOdo@gmail.com>
2022-09-24 23:15:38 +0900

    implement scrollable videobox

--- src/components/subjectPageComponents/VideosBox.tsx
+++ src/components/subjectPageComponents/VideosBox.tsx
@@ -91,6 +91,18 @@ type Props = {
   videos: Video[];
 };
 
+function secondsToHms(d: number) {
+  d = Number(d);
+  var h = Math.floor(d / 3600);
+  var m = Math.floor((d % 3600) / 60);
+  var s = Math.floor((d % 3600) % 60);
+
+  var hDisplay = h > 0 ? h + "h" : "";
+  var mDisplay = m > 0 ? m + "m" : "";
+  var sDisplay = s > 0 ? s + "s" : "";
+  return hDisplay + mDisplay + sDisplay;
+}
+
 export function VideosBox(propsVideo: Props) {
   const navigate = useNavigate();
   const location = useLocation();
@@ -107,8 +119,18 @@ export function VideosBox(propsVideo: Props) {
             }`}
             secondary={
               `${propsVideo.videos[index].faculty} ` +
-              `${propsVideo.videos[index].lecturedOn}`
+              `${propsVideo.videos[index].lecturedOn.slice(0, 10)} ` +
+              `${propsVideo.videos[index].language} ` +
+              `${secondsToHms(propsVideo.videos[index].videoLength)}`
             }
+            onClick={() => {
+              propsVideo.setFocusedVideoOrdering(
+                propsVideo.videos[index].ordering
+              );
+              navigate(
+                `/subjects/?id=${propsVideo.subject.id}&video=${propsVideo.videos[index].id}` //FIXME ad-hoc solution for routing
+              );
+            }}
           />
         </ListItemButton>
       </ListItem>
@@ -142,7 +164,7 @@ export function VideosBox(propsVideo: Props) {
 
       <FixedSizeList
         height={400}
-        width={360}
+        width={560}
         itemSize={100}
         itemCount={propsVideo.videos.length}
         overscanCount={100}
