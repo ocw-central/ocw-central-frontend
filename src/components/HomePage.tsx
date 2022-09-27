a046e227a4e57989ef105b97cca4e4cff102b38d
Yasutaka Odo <yasutakaodo@gmail.com>
2022-09-27 18:49:41 +0900

    display videos on HomePage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,14 +1,15 @@
 import { Box } from "@mui/material";
 
-import { Loading } from "@/components/common/Loading";
 import { SubjectCard } from "@/components/common/SubjectCard";
-import { useSubjectOnSearchPageQuery } from "@/generated/graphql";
-import { useRef, useState } from "react";
-import {
-  createSearchParams,
-  useNavigate,
-  useSearchParams,
-} from "react-router-dom";
+import { useSubjectOnHomepageQuery } from "@/generated/graphql";
+import { useNavigate } from "react-router-dom";
+
+type SubjectOnSearchPage = {
+  id: string;
+  title: string;
+  faculty: string;
+  thumbnailLink: string;
+};
 
 type Params = {
   title?: string;
@@ -16,27 +17,30 @@ type Params = {
   field?: string;
 };
 
-const ChangeGridItems = () => {
+const FEATURED_SUBJECTS_IDS = [
+  "01GB4X63H1KYQ3K8MN95PGYASY",
+  "01GBMW5CXV2RMJMSTQKCS5KKC0",
+  "01GB4X63GYPW8ECNVC7WXW7934",
+  "01GB4X63H5XDNBJ36VWYS83X9V",
+  "01GB4X63GY2TZ3RGR0FHZ25K53",
+  "01GB4X63GYCAFPK51DZZSKB8JF",
+  "01GB4X63H0451ZJJ7RS4FB7PT2",
+  "01GB4X63GZHN1NBB0Q7S84XH0A",
+  "01GB4X63H0D6B8FZJZS7FDHSM9",
+];
+
+export function HomePage() {
+  const navigate = useNavigate();
   const GridItems: JSX.Element[] = [];
-  const [searchParams] = useSearchParams();
-  const titleParam = searchParams.get("title");
-  const title: string = titleParam !== null ? titleParam : "";
-  const facultyParam = searchParams.get("faculty");
-  const faculty: string = facultyParam !== null ? facultyParam : "";
-  const academicFieldParam = searchParams.get("field");
-  const field: string = academicFieldParam !== null ? academicFieldParam : "";
-  const mounted = useRef(false);
-  const { data, loading, error } = useSubjectOnSearchPageQuery({
-    variables: {
-      title: title,
-      faculty: faculty,
-      field: field,
-    },
-    skip: title === "" && faculty === "" && field === "",
-  });
-  if (mounted.current) {
+  FEATURED_SUBJECTS_IDS.forEach((id) => {
+    const { data, loading, error } = useSubjectOnHomepageQuery({
+      variables: {
+        id: id,
+      },
+    });
+
     if (loading) {
-      return <Loading size={"7em"} color={"primary"} />;
+      return <div>Loading</div>;
     }
     if (error) {
       return <div>Error</div>;
@@ -45,37 +49,9 @@ const ChangeGridItems = () => {
       return <div>該当講義がありません</div>;
     }
     if (data) {
-      data.subjects.forEach((subject) => {
-        GridItems.push(<SubjectCard {...subject} />);
-      });
+      GridItems.push(<SubjectCard {...data.subject} />);
     }
-  }
-  mounted.current = true;
-  return GridItems;
-};
-
-export function HomePage() {
-  const navigate = useNavigate();
-  // クエリパラメータをもとに検索を行い、コンポーネントを書き換える
-  const GridItems = ChangeGridItems();
-
-  // 講義名検索結果を持つstate
-  const [searchTitle, setSearchTitle] = useState("");
-  const [searchFaculty, setSearchFaculty] = useState("");
-  const [searchAcademicField, setSearchAcademicField] = useState("");
-
-  // stateに基づきsearch parameterを切り替える関数
-  const setSearchParams = () => {
-    const params: Params = {
-      title: searchTitle,
-      faculty: searchFaculty,
-      field: searchAcademicField,
-    };
-    const searchParams = createSearchParams(params);
-
-    navigate(`?${searchParams}`);
-  };
-
+  });
   return (
     <Box>
       <Box className="HomePage" sx={{ flexGrow: 1 }}>
@@ -85,6 +61,10 @@ export function HomePage() {
           教育資源の保全と学習者の利便性の向上を目的としています。
           このサイトは京都大学情報学科の学生有志によって自主的に運営されています。
         </h1>
+      </Box>
+      <Box className="FeaturedSubjects">
+        <h2>Featured Subjects</h2>
+        {GridItems}
       </Box>
     </Box>
   );
