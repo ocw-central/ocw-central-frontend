74f9017b7e0228ef14486b065def258c782b97be
shibukazu <61775791+shibukazu@users.noreply.github.com>
2022-10-02 00:22:57 +0900

    modify homepage design (#83)

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,11 +1,13 @@
-import { Box } from "@mui/material";
+import { alpha, Box } from "@mui/material";
 
 import { Loading } from "@/components/common/Loading";
 import { SubjectCard } from "@/components/common/SubjectCard";
 import { useRandomSubjectQuery } from "@/generated/graphql";
 import { Divider, Grid, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-
+import { theme } from "@/utils/themes";
+import { ReactComponent as OwlImg } from "@/assets/ocwc-owl.svg";
+import { ReactComponent as LogoImg } from "@/assets/logo.svg";
 type SubjectOnSearchPage = {
   id: string;
   title: string;
@@ -49,45 +51,124 @@ export function HomePage() {
     });
   }
   return (
-    <Box>
-      <Box
-        className="HomePage"
-        sx={{ display: "flex", flexDirection: "column" }}
+    <Box
+      className="HomePage"
+      alignItems="center"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Grid
+        container
+        sx={{
+          height: {
+            xs: 300,
+            sm: 400,
+            md: 300,
+          },
+          width: {
+            xs: "100%",
+            md: "80%",
+          },
+          p: {
+            xs: 1,
+            md: 4,
+          },
+          backgroundColor: alpha(theme.palette.primary.main, 1),
+          borderColor: alpha(theme.palette.primary.main, 1),
+          borderWidth: 20,
+          borderRadius: 2,
+        }}
       >
-        <h2>
-          OCW
-          CentralはOCW(高等教育機関によってオープンライセンスの元で提供される無料の学習教材)のポータルサイトです。
-          教育資源の保全とアクセス向上を目的としています。
-          このサイトは京都大学情報学科の学生有志によって自主的に運営されており、いかなる大学機関とも関わりはありません。
-        </h2>
         <Grid
-          container
-          sx={{ height: "100%", alignSelf: "center", justifyContent: "center" }}
+          item
+          md={7}
+          sm={12}
+          xs={12}
+          sx={{
+            px: {
+              xs: 2,
+              md: 5,
+              ld: 10,
+            },
+            py: {
+              xs: 2,
+              md: 2,
+            },
+            m: {
+              xs: 2,
+              md: 0,
+            },
+          }}
         >
-          <Grid item xs={12} md={15}>
-            <Box
-              sx={{
-                m: {
-                  xs: 0,
-                  md: 4,
-                },
-                mb: 2,
-                display: "flex",
-                flexDirection: "column",
-                height: 700,
-                overflow: "hidden",
-                overflowY: "scroll",
-              }}
-            >
-              <Typography variant="h5" component="div" align="left">
-                <b>Feeling Lucky</b>
-              </Typography>
-              <Divider sx={{ mb: 2 }} />
-              <Grid container>{GridRandomItems}</Grid>
-            </Box>
-          </Grid>
+          <Typography
+            variant="h2"
+            sx={{
+              textAlign: "left",
+              color: "white",
+              mb: {
+                xs: 2,
+                md: 4,
+              },
+            }}
+          >
+            <b>Welcome to OCW Central!</b>
+          </Typography>
+          <Typography
+            variant="h5"
+            sx={{
+              textAlign: "left",
+              color: "white",
+              my: 4,
+            }}
+          >
+            <b>大学によって無償で提供される教育資料のポータルサイトです。</b>
+          </Typography>
         </Grid>
-      </Box>
+        <Grid
+          item
+          md={5}
+          xs={0}
+          sm={0}
+          sx={{
+            display: {
+              xs: "none",
+              sm: "none",
+              md: "block",
+            },
+          }}
+        >
+          <OwlImg style={{ width: "200px", height: "250px" }} />
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        sx={{ height: "100%", alignSelf: "center", justifyContent: "center" }}
+      >
+        <Grid item xs={12} md={15}>
+          <Box
+            sx={{
+              m: {
+                xs: 0,
+                md: 4,
+              },
+              mb: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: 700,
+              overflow: "hidden",
+              overflowY: "scroll",
+            }}
+          >
+            <Typography variant="h5" component="div" align="left">
+              <b>Feeling Lucky</b>
+            </Typography>
+            <Divider sx={{ mb: 2 }} />
+            <Grid container>{GridRandomItems}</Grid>
+          </Box>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
