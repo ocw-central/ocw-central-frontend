1cebba4f154f2a128675bd6de08bb87ba3867b6a
Yasutaka Odo <yasutakaodo@gmail.com>
2022-10-02 22:32:23 +0900

    add backgorund image for homepage

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,23 +1,9 @@
 import { Contact } from "@/components/common/Contact";
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 
 export function AboutPage() {
   return (
     <Box className="About" sx={{ m: "5em" }}>
-      <Typography
-        variant="h5"
-        sx={{
-          textAlign: "left",
-          my: 4,
-        }}
-      >
-        <b>
-          OCW
-          CentralはOCW(大学によって無償で提供される教育資料)のポータルサイトです。
-          <br></br>
-          京都大学情報学科の学生有志によって教育資源の保全とアクセス向上を目指して運営されています。
-        </b>
-      </Typography>
       <Contact />
     </Box>
   );
