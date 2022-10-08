02ed32f99a63ea5fec64ce617ec2778f2c04cb72
shibukazu <shibukazu@hotmail.co.jp>
2022-10-08 22:52:17 +0900

    refactored search page and implement transcript using real response

--- src/components/subjectPageComponents/VideoTranscription.tsx
+++ src/components/subjectPageComponents/VideoTranscription.tsx
@@ -32,23 +32,7 @@ export function VideoTranscription(props: Props) {
     return `${mm}:${ss}`;
   }
 
-  const dummy_text = `0,15,この授業は振動波動論の授業です
-15,17,それでは始めます
-17,21,最初に自己紹介をしておきます
-21,34,私の名前は前川悟と申します
-34,38,所属は人間環境学研究科で
-38,42,専門分野は低温磁性物理学ということで
-42,45,絶対冷凍近くに物質の温度を冷やして
-45,52,物質の磁気的な性質の研究を実験的に行っています
-52,58,それで私の研究室は
-58,62,この西の5階建ての建物の
-62,64,人間環境学研究科棟
-64,66,人間棟と言いますけれども
-66,73,人間棟のJ209号室におりますので
-73,76,質問とか話とかありましたら
-76,79,来ていただいたら結構です`;
-
-  const processedLines = processText(dummy_text);
+  const processedLines = processText(props.transcription);
 
   return (
     <Box
