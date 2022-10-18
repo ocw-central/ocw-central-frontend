5a818d5b96f798376e910ae9923e1749aa70cece
Yasutaka Odo <YasutakaOdo@gmail.com>
2022-10-18 23:34:20 +0900

    change thumbnail url

--- src/components/searchPageComponents/SubjectCard.tsx
+++ src/components/searchPageComponents/SubjectCard.tsx
@@ -21,15 +21,12 @@ const MyImageListItem = styled(ImageListItem)({
 });
 
 export const SubjectCard = (subject: SubjectOnSearchPage) => {
+  const thumbnailURL = "public/subject_thumbnails/" + subject.id + ".png";
   return (
     <Grid item xs={12} sm={4} md={4} sx={{ p: 0.5 }}>
       <Link to={`/subjects/${subject.id}`}>
         <MyImageListItem>
-          <img
-            src={`${subject.thumbnailLink}`}
-            loading="lazy"
-            style={{ height: 280 }}
-          />
+          <img src={thumbnailURL} loading="lazy" style={{ height: 280 }} />
           <MyImageListItemBar
             title={subject.title}
             subtitle={subject.faculty}
