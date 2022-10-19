c61a5b1891791542cdc883d191e3baba7a668c91
shibukazu <shibukazu@hotmail.co.jp>
2022-10-19 20:35:18 +0900

    modified 404 page color

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -1,12 +1,21 @@
+import { theme } from "@/utils/themes";
 import { Box, Typography } from "@mui/material";
 
 export function PageNotFound() {
   return (
     <Box>
-      <Typography variant="h2" align="center" sx={{ color: "black" }}>
+      <Typography
+        variant="h2"
+        align="center"
+        sx={{ color: theme.palette.primary.dark }}
+      >
         404 Not Found
       </Typography>
-      <Typography variant="h3" align="center" sx={{ color: "black" }}>
+      <Typography
+        variant="h3"
+        align="center"
+        sx={{ color: theme.palette.primary.dark }}
+      >
         お探しのページは見つかりませんでした。
       </Typography>
     </Box>
