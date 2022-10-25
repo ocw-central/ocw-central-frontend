8b9ee7b64e3c4dd0f9e23bb275364691ef313557
Yasutaka Odo <YasutakaOdo@gmail.com>
2022-10-26 02:11:42 +0900

    remove subject specific ogp tags

--- src/components/common/OGPTag.tsx
+++ src/components/common/OGPTag.tsx
@@ -19,21 +19,16 @@ export default function OGPTag({
 
   return (
     <Helmet>
-      <meta property="og:title" content={title || metadata.siteTitle} />
+      <meta property="title" content={title || metadata.siteTitle} />
       <meta
-        property="og:description"
+        property="description"
         content={description || metadata.siteDescription}
       />
 
-      <meta property="og:url" content={metadata.siteUrl} />
-      <meta property="og:type" content={type} />
-      <meta property="og:site_name" content={metadata.siteTitle} />
-      <meta property="og:image" content={image_url || metadata.siteImageUrl} />
-      <meta name="twitter:card" content="summary" />
-      <meta
-        name="twitter:site"
-        content={`@${metadata.siteAuthorTwitterUserName}`}
-      />
+      <meta property="url" content={metadata.siteUrl} />
+      <meta property="type" content={type} />
+      <meta property="site_name" content={metadata.siteTitle} />
+      <meta property="image" content={image_url || metadata.siteImageUrl} />
     </Helmet>
   );
 }
