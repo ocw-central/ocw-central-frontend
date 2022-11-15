47d081da301b46d6f81d28233ca5876a3f9e52e4
Yasutaka Odo <YasutakaOdo@gmail.com>
2022-11-16 02:59:49 +0900

    translate syllabus and subject details

--- src/components/subjectPageComponents/SubjectDetails.tsx
+++ src/components/subjectPageComponents/SubjectDetails.tsx
@@ -1,3 +1,4 @@
+import { Subject } from "@/gqltypes/subject";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableCell from "@mui/material/TableCell";
@@ -5,9 +6,10 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { Box } from "@mui/system";
-import { Subject } from "@/gqltypes/subject";
+import { useTranslation } from "react-i18next";
 
 export function SubjectDetails({ subject }: { subject: Subject }) {
+  const { t } = useTranslation();
   return (
     <Box>
       <TableContainer component={Paper}>
@@ -16,47 +18,47 @@ export function SubjectDetails({ subject }: { subject: Subject }) {
             <TableRow>
               {subject.firstHeldOn && (
                 <TableCell>
-                  <b>開講日</b>
+                  <b>{t("translation.subject.details.first_held_on")}</b>
                 </TableCell>
               )}
               {subject.category && (
                 <TableCell>
-                  <b>カテゴリ</b>
+                  <b>{t("translation.subject.details.category")}</b>
                 </TableCell>
               )}
               {subject.location && (
                 <TableCell>
-                  <b>開催場所</b>
+                  <b>{t("translation.subject.details.location")}</b>
                 </TableCell>
               )}
               {subject.department && (
                 <TableCell>
-                  <b>開講学科</b>
+                  <b>{t("translation.subject.details.department")}</b>
                 </TableCell>
               )}
               {subject.faculty && (
                 <TableCell>
-                  <b>担当教員</b>
+                  <b>{t("translation.subject.details.faculty")}</b>
                 </TableCell>
               )}
               {subject.language && (
                 <TableCell>
-                  <b>言語</b>
+                  <b>{t("translation.subject.details.language")}</b>
                 </TableCell>
               )}
               {subject.freeDescription && (
                 <TableCell>
-                  <b>概要</b>
+                  <b>{t("translation.subject.details.free_description")}</b>
                 </TableCell>
               )}
               {subject.series && (
                 <TableCell>
-                  <b>シリーズ</b>
+                  <b>{t("translation.subject.details.series")}</b>
                 </TableCell>
               )}
               {subject.academicField && (
                 <TableCell>
-                  <b>学術分野</b>
+                  <b>{t("translation.subject.details.academic_field")}</b>
                 </TableCell>
               )}
             </TableRow>
